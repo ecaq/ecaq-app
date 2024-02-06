a7c542121d6512c389f17246d5d0e2f046934932
Felix Matusinio <FM2150666@aabqatar.com>
2024-02-06 19:34:48 +0300

    disable fetch member

--- src/app/stores/memberStore..ts
+++ src/app/stores/memberStore..ts
@@ -1,6 +1,6 @@
-import { makeAutoObservable, runInAction } from "mobx";
+import { makeAutoObservable } from "mobx";
 import { Member } from "../models/member";
-import agent from "../api/agent";
+//import agent from "../api/agent";
 
 export default class MemberStore {
     members: Member[] = []
@@ -17,59 +17,59 @@ export default class MemberStore {
         this.loadingInitial = state
     }
     
-    loadMembers = async () => {
-        console.log("loadmemebrs call")
-        this.setLoadingInitial(true)
-        try {
-            const members = await agent.Members.list()
-            console.log("loadmemebrs call", members)
-            //runInAction(() => {})
-                members.forEach(member => {                
-                    //this.setMember(member)
-                    // member.joinDate =  member.joinDate?.split('T')[0]
-                    this.members.push(member)
-                })                
+    // loadMembers = async () => {
+    //     console.log("loadmemebrs call")
+    //     this.setLoadingInitial(true)
+    //     try {
+    //         const members = await agent.Members.list()
+    //         console.log("loadmemebrs call", members)
+    //         //runInAction(() => {})
+    //             members.forEach(member => {                
+    //                 //this.setMember(member)
+    //                 // member.joinDate =  member.joinDate?.split('T')[0]
+    //                 this.members.push(member)
+    //             })                
                       
-            this.setLoadingInitial(false)
-        } catch (error) {
-            console.log(error)
-            //runInAction(() => {})
-            this.setLoadingInitial(false)            
-        }
-    }
+    //         this.setLoadingInitial(false)
+    //     } catch (error) {
+    //         console.log(error)
+    //         //runInAction(() => {})
+    //         this.setLoadingInitial(false)            
+    //     }
+    // }
 
-    loadMember = async (id: number) => {
-        let member = this.getMember(id)
-        if(member) {
-            this.selectedMember = member
-            return member
-        } else {
-            try {
-                member = await agent.Members.details(id)
-                this.setMember(member)
-                runInAction(() => this.selectedMember = member)                            
-                this.setLoadingInitial(false)
-                return member
-            } catch (error) {
-                console.log(error)
-                //runInAction(() => {})
-                this.setLoadingInitial(false)            
-            }
-        }
+    // loadMember = async (id: number) => {
+    //     let member = this.getMember(id)
+    //     if(member) {
+    //         this.selectedMember = member
+    //         return member
+    //     } else {
+    //         try {
+    //             member = await agent.Members.details(id)
+    //             this.setMember(member)
+    //             runInAction(() => this.selectedMember = member)                            
+    //             this.setLoadingInitial(false)
+    //             return member
+    //         } catch (error) {
+    //             console.log(error)
+    //             //runInAction(() => {})
+    //             this.setLoadingInitial(false)            
+    //         }
+    //     }
 
-    }
+    // }
 
-    private setMember = (member: Member) => {
-        //activity.date = new Date(activity.date!)
-        console.log("setmember call", member)
-        this.members?.push(member)
-    }
-    private getMember = (id: number) => {
-        return this.members?.find(x => x.id === id)
-        // return this.members?.find((obj) => {
-        //     return obj.id === id;
-        //   });
-    }
+    // private setMember = (member: Member) => {
+    //     //activity.date = new Date(activity.date!)
+    //     console.log("setmember call", member)
+    //     this.members?.push(member)
+    // }
+    // private getMember = (id: number) => {
+    //     return this.members?.find(x => x.id === id)
+    //     // return this.members?.find((obj) => {
+    //     //     return obj.id === id;
+    //     //   });
+    // }
 
 
 
