b5c6dbb5d3baaab6fcaaa1eab3ae6a3e5e7fcfd0
Felix Matusinio <FM2150666@aabqatar.com>
2024-02-06 19:04:13 +0300

    stroke- style modified

--- src/features/components/SwiperSlider.tsx
+++ src/features/components/SwiperSlider.tsx
@@ -191,13 +191,13 @@ export default function SwiperSlider() {
                         </Link>
                         <br />
                         <SwiperButtonPrev>
-                          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-6 h-6">
-                            <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 19.5 8.25 12l7.5-7.5" />
+                          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6">
+                            <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 19.5 8.25 12l7.5-7.5" />
                           </svg>
                         </SwiperButtonPrev>      
                         <SwiperButtonNext>
-                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-6 h-6">
-                              <path stroke-linecap="round" stroke-linejoin="round" d="m8.25 4.5 7.5 7.5-7.5 7.5" />
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6">
+                              <path strokeLinecap="round" strokeLinejoin="round" d="m8.25 4.5 7.5 7.5-7.5 7.5" />
                             </svg>
                           </SwiperButtonNext>
                     </div>
