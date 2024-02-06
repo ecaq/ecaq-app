d7940a3b2f48c11256aa9ab41432e8c38cdde478
Felix Matusinio <FM2150666@aabqatar.com>
2024-02-06 18:56:13 +0300

    temporary disable map page

--- src/features/map/MapLayout.tsx
+++ src/features/map/MapLayout.tsx
@@ -1,26 +1,26 @@
-import { useEffect } from "react";
-import MemberList from "./MemberList";
-import { useStore } from "../../app/stores/store";
+//import { useEffect } from "react";
+//import MemberList from "./MemberList";
+//import { useStore } from "../../app/stores/store";
 import { observer } from "mobx-react-lite";
-import MapGoogle from "./MapGoogle";
-import LoadingComponent from "../components/LoadingComponent";
+//import MapGoogle from "./MapGoogle";
+//import LoadingComponent from "../components/LoadingComponent";
 
 export default observer(function MapLayout() {
-  const { memberStore } = useStore();
-  const { loadMembers, members } = memberStore;
+  // const { memberStore } = useStore();
+  // const { loadMembers, members } = memberStore;
 
-  useEffect(() => {
-    console.log("mmmm");
-    if (members.length === 0) {
+  // useEffect(() => {
+  //   console.log("mmmm");
+  //   if (members.length === 0) {
         
-        console.log("0");
-      loadMembers();
-    }
+  //       console.log("0");
+  //     loadMembers();
+  //   }
     
-    //console.log("mem", members);
-  }, [members, loadMembers]);
+  //   //console.log("mem", members);
+  // }, [members, loadMembers]);
 
-  if (memberStore.loadingInitial) return <LoadingComponent />;
+  // if (memberStore.loadingInitial) return <LoadingComponent />;
 
   return (
     // <div className="flex">
@@ -34,12 +34,12 @@ export default observer(function MapLayout() {
       <div className="flex h-screen">
         <div className="flex-1 space-y-8 p-12">
           <div>
-            <MemberList members={members} />
+            {/* <MemberList members={members} /> */}
           </div>
         </div>
 
         <div className="">
-          <MapGoogle members={members} />
+          {/* <MapGoogle members={members} /> */}
         </div>
       </div>
     </div>
