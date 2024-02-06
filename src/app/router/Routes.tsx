64b75300f83efe4d434f53e82d94d5da3435b003
Felix Matusinio <FM2150666@aabqatar.com>
2024-02-07 02:05:01 +0300

    added router lazyload and suspense

--- src/app/router/Routes.tsx
+++ src/app/router/Routes.tsx
@@ -1,13 +1,13 @@
 import { RouteObject, createBrowserRouter } from "react-router-dom";
-import App from "../layout/App";
-import Home from "../../features/home/Home";
-import MarketPalce from "../../features/market-place/MarketPlace";
-import MapLayout from "../../features/map/MapLayout";
-import Gallery from "../../features/gallery/Gallery";
-import EcaqMissionVision from "../../features/about/EcaqMissionVision";
-import StatementOfFaith from "../../features/about/StatementOfFaith";
-import ContactUs from "../../features/contact/ContactUs";
-import EcaqCore from "../../features/about/EcaqCore";
+import { lazy } from "react";
+
+const App = lazy(() => import("../layout/App"))
+const Home = lazy(() => import("../../features/home/Home"))
+const Gallery = lazy(() => import("../../features/gallery/Gallery"))
+const EcaqMissionVision = lazy(() => import("../../features/about/EcaqMissionVision"))
+const StatementOfFaith = lazy(() => import("../../features/about/StatementOfFaith"))
+const ContactUs = lazy(() => import("../../features/about/StatementOfFaith"))
+const EcaqCore = lazy(() => import("../../features/about/EcaqCore"))
 
 export const routes: RouteObject[] = [
   {
@@ -33,14 +33,6 @@ export const routes: RouteObject[] = [
       {
         path: "/gallery",
         element: <Gallery />,
-      },
-      {
-        path: "/market-place",
-        element: <MarketPalce />,
-      },
-      {
-        path: "/map",
-        element: <MapLayout />,
       },
       {
         path: "/contact-us",
--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,16 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { RouterProvider } from 'react-router-dom'
-import { router } from './app/router/Routes.tsx'
-import { StoreContext, store } from './app/stores/store.ts'
-import './index.css'
+import React, { Suspense } from "react";
+import ReactDOM from "react-dom/client";
+import { RouterProvider } from "react-router-dom";
+import { router } from "./app/router/Routes.tsx";
+import { StoreContext, store } from "./app/stores/store.ts";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <StoreContext.Provider value={store}>
-      <RouterProvider router={router} />
+      <Suspense>
+        <RouterProvider router={router} />
+      </Suspense>
     </StoreContext.Provider>
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
